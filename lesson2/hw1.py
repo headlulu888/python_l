bb357549de446b3c6712c839a0f26433ac27335f
headlulu888 <headlulu888@gmail.com>
2019-06-13 17:57:31 +0300

    hw2 task2

--- lesson2/hw1.py
+++ lesson2/hw1.py
@@ -1,3 +1,15 @@
 # first task
-list = [10, 12, 232, 94, 1, 77, 4]
-print(sorted(list))
+# list = [10, 12, 232, 94, 1, 77, 4]
+# print(sorted(list))
+
+# second task
+dict = {
+	1: 'hello',
+	2: 'world',
+	6: 'testing_value',
+	10: 'None',
+	3: 'last_value'
+}
+
+for key, value in dict.items():
+	print('Key is: {} - Value is: {}'.format(key, value))
